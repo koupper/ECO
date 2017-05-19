e6edc9cba28ba3f4364712583adf728c5b7b2e49
Unknown <koupperrn@live.com>
2017-05-19 21:20:35 +0800

    PREFAB LISTO DE CASILLAS

---
+++ casilla eco/Assets/Scripts/Secar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secar : MonoBehaviour {
+	public Material arbolVivo;
+	public Material arbolMuerto;
+    public Material vacio;
+    public float secar = 5f;
+
+	// Use this for initialization
+	public void Start () {
+		gameObject.GetComponent<Renderer>().material = vacio;
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        secar = secar + 1 + Time.fixedDeltaTime;
+        if(secar > 500)
+            gameObject.GetComponent<Renderer>().material = arbolVivo;
+        if (Input.GetKeyDown ("space"))
+			gameObject.GetComponent<Renderer>().material = arbolMuerto;
+		
+	}
+}
